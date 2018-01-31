3d384327ecab99fa53889057920a0421c04c0234
liguobin <liguobin@siasun.com>
2018-01-31 08:48:30 +0800

    消除rdf_loader和robot_model_loader依赖，直接构建RobotModel,RobotState和Kinematic_loader，默认为kdl，编译通过，需进一步构建查找库，动态实例化kdl插件

--- kinematic_model.cpp
+++ kinematic_model.cpp
@@ -8,6 +8,7 @@
 #include <moveit/robot_state/robot_state.h>
 #include <console_bridge/console.h>
 #include <iostream>
+#include <memory>
 
 int main(int argc, char **argv)
 {
@@ -31,9 +32,85 @@ int main(int argc, char **argv)
     // :moveit_core:`RobotModel` for us to use.
     //
     // .. _RobotModelLoader: http://docs.ros.org/indigo/api/moveit_ros_planning/html/classrobot__model__loader_1_1RobotModelLoader.html
-    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-    robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+    //robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+    //ModelInterfaceSharedPtr urdf_model(new ModelInterface());
+    console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_DEBUG);
+    std::string urdf_file("srcz.urdf");
+    std::string srdf_file("srcz.srdf");
+    auto umodel = new urdf::Model();
+    auto urdf_model = std::make_shared<urdf::ModelInterface>();
+    urdf_model.reset(umodel);
+    umodel->initFile(urdf_file);
+
+    auto srdf_model = std::make_shared<srdf::Model>();
+    srdf_model->initFile(*urdf_model, srdf_file);
+
+
+    auto kinematic_model = std::make_shared<robot_model::RobotModel>(urdf_model, srdf_model);
     CONSOLE_BRIDGE_logInform("Model frame: %s", kinematic_model->getModelFrame().c_str());
+
+    kinematics_plugin_loader::KinematicsPluginLoaderPtr kinematics_loader(
+            new kinematics_plugin_loader::KinematicsPluginLoader());
+    CONSOLE_BRIDGE_logDebug("1");
+    robot_model::SolverAllocatorFn kinematics_allocator = kinematics_loader->getLoaderFunction(srdf_model);
+    CONSOLE_BRIDGE_logDebug("2");
+    const std::vector<std::string>& groups = kinematics_loader->getKnownGroups();
+    CONSOLE_BRIDGE_logDebug("3");
+    std::stringstream ss;
+    std::copy(groups.begin(), groups.end(), std::ostream_iterator<std::string>(ss, " "));
+    CONSOLE_BRIDGE_logDebug("Loaded information about the following groups: %s" ,ss.str().c_str() );
+
+    std::map<std::string, robot_model::SolverAllocatorFn> imap;
+    CONSOLE_BRIDGE_logDebug("4");
+    for (std::size_t i = 0; i < groups.size(); ++i)
+    {
+        // Check if a group in kinematics.yaml exists in the srdf
+        if (!kinematic_model->hasJointModelGroup(groups[i]))
+            continue;
+
+        const robot_model::JointModelGroup* jmg = kinematic_model->getJointModelGroup(groups[i]);
+        CONSOLE_BRIDGE_logDebug("5");
+        kinematics::KinematicsBasePtr solver = kinematics_allocator(jmg);
+        CONSOLE_BRIDGE_logDebug("6");
+        if (solver)
+        {
+            std::string error_msg;
+            if (solver->supportsGroup(jmg, &error_msg))
+            {
+                imap[groups[i]] = kinematics_allocator;
+            }
+            else
+            {
+                CONSOLE_BRIDGE_logError("Kinematics solver %s does not support joint group %s.  Error: %s", typeid(*solver).name(),
+                                        groups[i].c_str(), error_msg.c_str());
+            }
+        }
+        else
+        {
+            CONSOLE_BRIDGE_logError("Kinematics solver could not be instantiated for joint group %s.", groups[i].c_str());
+        }
+    }
+    kinematic_model->setKinematicsAllocators(imap);
+
+    // set the default IK timeouts
+    const std::map<std::string, double>& timeout = kinematics_loader->getIKTimeout();
+    for (std::map<std::string, double>::const_iterator it = timeout.begin(); it != timeout.end(); ++it)
+    {
+        if (!kinematic_model->hasJointModelGroup(it->first))
+            continue;
+        robot_model::JointModelGroup* jmg = kinematic_model->getJointModelGroup(it->first);
+        jmg->setDefaultIKTimeout(it->second);
+    }
+
+    // set the default IK attempts
+    const std::map<std::string, unsigned int>& attempts = kinematics_loader->getIKAttempts();
+    for (std::map<std::string, unsigned int>::const_iterator it = attempts.begin(); it != attempts.end(); ++it) {
+        if (!kinematic_model->hasJointModelGroup(it->first))
+            continue;
+        robot_model::JointModelGroup *jmg = kinematic_model->getJointModelGroup(it->first);
+        jmg->setDefaultIKAttempts(it->second);
+    }
+
 
     // Using the :moveit_core:`RobotModel`, we can construct a
     // :moveit_core:`RobotState` that maintains the configuration
@@ -44,7 +121,7 @@ int main(int argc, char **argv)
     // robot.
     robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
     kinematic_state->setToDefaultValues();
-    const robot_state::JointModelGroup *joint_model_group = kinematic_model->getJointModelGroup("right_arm");
+    const robot_state::JointModelGroup *joint_model_group = kinematic_model->getJointModelGroup("arm");
 
     const std::vector<std::string> &joint_names = joint_model_group->getVariableNames();
 
@@ -94,7 +171,7 @@ int main(int argc, char **argv)
     // "r_wrist_roll_link" which is the most distal link in the
     // "right_arm" of the robot.
     kinematic_state->setToRandomPositions(joint_model_group);
-    const Eigen::Affine3d &end_effector_state = kinematic_state->getGlobalLinkTransform("r_wrist_roll_link");
+    const Eigen::Affine3d &end_effector_state = kinematic_state->getGlobalLinkTransform("L6");
 
     /* Print end-effector pose. Remember that this is in the model frame */
     std::cout << end_effector_state.translation() << std::endl;
